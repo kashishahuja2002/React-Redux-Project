39a69eed3240016bba25915d95199ccc38311474
kashishahuja2002 <kashishahuja2002@gmail.com>
2022-06-13 10:32:03 +0530

    Updated createStore

--- src/Store.js
+++ src/Store.js
@@ -1,5 +1,5 @@
 import thunk from 'redux-thunk';
-import { createStore } from 'redux';
+import { legacy_createStore as createStore } from 'redux';
 import { applyMiddleware } from 'redux';
 import RootReducer from './Reducer/RootReducer';
 
